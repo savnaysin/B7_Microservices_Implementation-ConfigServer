d0b64357135d5dd64f071345a6454397902ba4a5
Savinay Singh <savinay.singh86@gmail.com>
2019-09-15 01:00:08 -0500

    new property values

--- src/main/java/b7/savsi/implementation/SpringConfigServer/SpringConfigServerApplication.java
+++ src/main/java/b7/savsi/implementation/SpringConfigServer/SpringConfigServerApplication.java
@@ -3,6 +3,7 @@ package b7.savsi.implementation.SpringConfigServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+
 @EnableConfigServer
 @SpringBootApplication
 public class SpringConfigServerApplication {
